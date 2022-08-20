ecc0542e89a39fd2ea65972e85f526a1e2843edf
Heleno Nascimento <heleno_55@hotmail.com>
2022-08-20 12:47:51 -0300

    primeiro commit do clone netflix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [movieList, setMovieList] = useState("");
+
+    useEffect(() => {
+      const loadAll = async () =>{
+          // Pegando a lista total
+
+          let list = await Tmdb.getHomeList();
+          setMovieList(list);
+      }
+
+      loadAll();
+    }, []);
+    // 
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <section className= "lists"> 
+      {movieList && movieList.map((item, key) => (
+           <MovieRow key={key} title={item.title} items={item.items}/>
+          
+      ))}
+     </section>
     </div>
   );
 }
